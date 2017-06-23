13f20f6291594cabb48d216b326888a6e398eea1
youpy <youpy@buycheapviagraonlinenow.com>
2017-06-23 12:14:59 +0900

    update mongoid

--- collaborator.rb
+++ collaborator.rb
@@ -1,19 +1,13 @@
-if mongo_uri = ENV['MONGODB_URI']
-  Mongoid.database = Mongo::Connection.from_uri(mongo_uri).
-    db(URI.parse(mongo_uri).path.gsub(/^\//, ''))
-else
-  host = 'localhost'
-  port = Mongo::Connection::DEFAULT_PORT
-  Mongoid.database = Mongo::Connection.new(host, port).db('twitter_access_tokens')
-end
+Mongoid.load!('mongoid.yml', ENV['MONGODB_URI'] ? :production : :development)
 
 class Collaborator
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :id_str, :type => String
   field :key, :type => String
   field :secret, :type => String
-  index :id_str, :unique => true
+  index({ id_str: 1 }, { unique: true })
 
   def tweet(c, consumer_key, consumer_secret)
     Twitter.configure do |config|
